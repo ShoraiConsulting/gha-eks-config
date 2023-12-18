514f11dfebeb8bee260217678666efb9ac2e86b6
Tom Lagodzinski <gothrek22@users.noreply.github.com>
2023-12-18 14:45:14 +0100

    Switch to inputs based envs.

--- kubeconfig.sh
+++ kubeconfig.sh
@@ -2,15 +2,15 @@
 
 TEMPDIR=$(mktemp -d)
 
-echo $CA >$TEMPDIR/ca.crt
+echo $INPUTS_CA >$TEMPDIR/ca.crt
 
 kubectl config set-context \
   --cluster=current \
   --namespace=default \
   --user=gha \
   --certificate-authority=$TEMPDIR/ca.crt \
-  --server=$URL \
-  --token=$TOKEN \
-  --username=$UNAME
+  --server=$INPUTS_SERVER \
+  --token=$INPUTS_TOKEN \
+  --username=$INPUTS_USER
 
 echo "K8S_TEMP=$TEMPDIR" >> $GITHUB_ENV
